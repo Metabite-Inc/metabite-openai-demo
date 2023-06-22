3eee9ee7c1e9c865a06d7e643c18340ceb7b4243
jenna-metabite <134967503+jenna-metabite@users.noreply.github.com>
2023-06-22 10:19:49 +0000

    change example questions

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "Do I need to eat breakfast?",
+        value: "Do I need to eat breakfast?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "Is it OK to drink coffee?", value: "Is it OK to drink coffee?" },
+    { text: "Is fish oil helpful for recovery from exercise?", value: "Is fish oil helpful for recovery from exercise?" }
 ];
 
 interface Props {
